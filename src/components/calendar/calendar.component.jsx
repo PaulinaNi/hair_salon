36593c001890b7d2f04cbdd85472c724e11f7845
Paulina <paulina.justyna.niedzielska@gmail.com>
2023-10-14 01:04:39 +0100

    Calendar component

--- src/components/calendar/calendar.component.jsx
+++ src/components/calendar/calendar.component.jsx
@@ -1,24 +1,54 @@
+import { useState } from "react"
 import "./calendar.style.css"
 
 export default function Calendar() {
 
+  const [displayMonth, setDisplayMonth] = useState(new Date())
+
   const todayDate = new Date()
   const thisMonth = todayDate.getMonth()
-  const lastDayOfMonth = new Date(todayDate.getFullYear(), todayDate.getMonth()+1,0).getDate()
-  const todayDay = todayDate.getDay()
-  const dayOfMonth = todayDate.getDate()
+  const lastDayOfMonth = new Date(todayDate.getFullYear(), todayDate.getMonth() + 1, 0)
+  const firstDayofMonth = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1).getDay()
+  const lastDayOfPrevMonth = new Date(todayDate.getFullYear(), todayDate.getMonth(), 0).getDate()
 
   const monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
   const daysArray = []
 
-  for (let i = 1; i <= lastDayOfMonth; i++) {
-    daysArray.push(i)
+  //load days to arrey from prev month
+  for (let i = lastDayOfPrevMonth - firstDayofMonth + 1; i <= lastDayOfPrevMonth; i++) {
+    daysArray.push({ dayNumber: i, class: 'day prev-next', isAvaliableForBooking: false })
+  }
+  //load days to arrey from this month
+  for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
+    if (i > todayDate.getDate()) {
+      daysArray.push({ dayNumber: i, class: 'day thisMonth avaliableForBooking', isAvaliableForBooking: true })
+    } else {
+      daysArray.push({ dayNumber: i, class: 'day thisMonth', isAvaliableForBooking: false })
+    }
+  }
+  //load days to arrey from next month
+  for (let i = 1; i <= 6 - lastDayOfMonth.getDay(); i++) {
+    daysArray.push({ dayNumber: i, class: 'day prev-next avaliableForBooking', isAvaliableForBooking: true })
+  }
+
+  const dayPickingHandler = (isAvaliableForBooking) => {
+    if (isAvaliableForBooking) {
+      console.log('working')
+    }
+  }
+
+  const toNextMonthHandler = (prevOrNext) => {
+    prevOrNext === 'prev' ? todayDate.setMonth(todayDate.getMonth() - 1) : todayDate.setMonth(todayDate.getMonth() + 1)
+    console.log(todayDate)
   }
 
   return (
     <div className="calendarContainer" >
       <h2 className="month">{monthsArray[thisMonth]}</h2>
-
+      <div className="calendarNavigation" >
+        <div onClick={() => toNextMonthHandler('prev')}>Previous Month</div>
+        <div onClick={() => toNextMonthHandler('next')}>Next Month</div>
+      </div>
       <div className="weekdays">
         <div>Sun</div>
         <div>Mon</div>
@@ -30,7 +60,13 @@ export default function Calendar() {
       </div>
 
       <div className="days">
-        {daysArray.map(day => <div className="day" key={day}>{day}</div> )}
+        {daysArray.map((day, index) =>
+          <div
+            className={day.class}
+            key={index}
+            onClick={() => dayPickingHandler(day.isAvaliableForBooking)}>
+            {day.dayNumber}
+          </div>)}
       </div>
     </div>
   )
